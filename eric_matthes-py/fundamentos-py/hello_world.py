d86686a85bd876508373b3ed107edd9d141bcff9
antoniosilv-l <antonio.lucas2011@hotmail.com>
2023-06-20 21:26:08 -0300

    Introdução ao python e funções básicas

--- eric_matthes-py/fundamentos-py/hello_world.py
+++ eric_matthes-py/fundamentos-py/hello_world.py
@@ -42,3 +42,34 @@ print(espaco_esquerda)
 espaco_ambos_lados = ' Espaço em ambos os lados '
 espaco_ambos_lados = espaco_ambos_lados.strip() ## Remoção de espaço em branco em ambos lados
 print(espaco_ambos_lados)
+
+##
+## Trabalhando com inteiros
+
+print(2 + 3) ## Somar
+print(3 - 2) ## Subtrair
+print(2 * 3) ## Multiplicar
+print(3 / 2) ## Dividir
+
+print(3 ** 2) ## Exponenciais
+print(3 ** 3) ## Exponenciais
+print(10 ** 6) ## Exponenciais
+
+print(2 + 3*4)
+print((2 + 3)*4)
+
+## Trabalhando com números decimais
+
+print( 0.1 + 0.1) 
+print( 0.2 + 0.2)
+print( 2 + 0.1)
+print( 2 + 0.2)
+
+print( 0.2 + 0.1)
+print( 3 * 0.1)
+
+## Função str()
+
+age = 23
+message = 'Happy ' + str(age) + 'rd Birthday!'
+print(message)
