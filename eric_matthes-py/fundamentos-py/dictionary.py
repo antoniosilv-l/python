c02c034cdaddc7603538522c1c28db9f88d4be1a
Antonio Silva <antonio.lucas2011@hotmail.com>
2023-10-30 22:14:08 -0300

    Ultimos estudos

--- eric_matthes-py/fundamentos-py/dictionary.py
+++ eric_matthes-py/fundamentos-py/dictionary.py
@@ -145,3 +145,48 @@ print('\nThe following languages have been mentioned: (Sem Duplicidade)')
 for language in set(favorite_language.values()):
     print(language.title())
 
+
+## Dicionarios Aninhados
+print('----')
+print('Criando uma lista de dicionarios.')
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+
+print('----')
+print('Criando varios itens de um dicionario de uma vez.')
+
+aliens = []
+
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:5]:
+    print(alien)
+print('...')
+
+print('----')
+print('Alterando alguns itens de um dicionario.')
+
+print('Total number of aliens: ' + str(len(aliens)))
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15        
+
+for alien in aliens[0:5]:
+    print(alien)
+print('...')
