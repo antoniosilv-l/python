8927d4644572e81d8c44c81ffc8a281406121f0b
Antonio Silva <antonio.lucas2011@hotmail.com>
2024-01-09 22:12:30 -0300

    Mudança de nomenclatura e estudo final do while

--- eric_matthes-py/1.fundamentos-py/while.py
+++ eric_matthes-py/1.fundamentos-py/while.py
@@ -75,11 +75,56 @@ prompt += "\nEnter 'quit' to end the program. "
 
 ## Utilizando o continue para retornar o inicio
 
-current_number = 0
-while current_number < 10:
-    current_number += 1
+# current_number = 0
+# while current_number < 10:
+#     current_number += 1
     
-    if current_number % 2 == 0:
-        continue
+#     if current_number % 2 == 0:
+#         continue
 
-    print(current_number)
+#     print(current_number)
+
+## Transferindo dados de uma lista para outra utilizando o while
+
+# unconfirmed_users = ['alice', 'brian', 'candace']
+# confirmed_users = []
+
+# while unconfirmed_users:
+#     current_user = unconfirmed_users.pop()
+
+#     print('Verifyng user: ' + current_user.title())
+#     confirmed_users.append(current_user)
+
+# print('\n The following users have been confirmed:')
+# for confirmed_user in confirmed_users:
+#     print(confirmed_user.title())
+
+## Removendo todas as instancias de uma lista com o while
+
+# pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+# print(pets)
+
+# while 'cat' in pets:
+#     pets.remove('cat')
+
+# print(pets)
+
+## Armazenando dados de um input em um dicionario
+
+responses = {}
+
+# Define uma flag para indicar que a enquete está ativa
+polling_active = True
+
+while polling_active:
+    name = input('\nWhat is your name? ')
+    response = input('Which mountain would you like to climb someday? ')
+    responses[name] = response
+
+    repeat = input('Would you like to let another person respond? (yes/no)')
+    if repeat == 'no':
+        polling_active = False
+
+print('\--- Poll Results ---')
+for name, response in responses.items():
+    print(name + ' would like to climb ' + response + '.')
